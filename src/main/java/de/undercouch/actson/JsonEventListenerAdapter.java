5771bc8ad91b5d2a14b0e5f388672444299f89e7
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2016-08-16 23:13:44 +0200

    Add adapter class for event listener

---
+++ src/main/java/de/undercouch/actson/JsonEventListenerAdapter.java
@@ -0,0 +1,82 @@
+// MIT License
+//
+// Copyright (c) 2016 Michel Kraemer
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package de.undercouch.actson;
+
+/**
+ * Adapter class providing empty implementations for all
+ * {@link JsonEventListener} methods. Can be used as a base class
+ * for implementations that are not interested in all JSON events.
+ * @author Michel Kraemer
+ */
+public class JsonEventListenerAdapter implements JsonEventListener {
+  @Override
+  public void onStartObject() {
+    // empty
+  }
+
+  @Override
+  public void onEndObject() {
+    // empty
+  }
+
+  @Override
+  public void onStartArray() {
+    // empty
+  }
+
+  @Override
+  public void onEndArray() {
+    // empty
+  }
+
+  @Override
+  public void onFieldName(String name) {
+    // empty
+  }
+
+  @Override
+  public void onValue(String value) {
+    // empty
+  }
+
+  @Override
+  public void onValue(int value) {
+    // empty
+  }
+
+  @Override
+  public void onValue(double value) {
+    // empty
+  }
+
+  @Override
+  public void onValue(boolean value) {
+    // empty
+  }
+
+  @Override
+  public void onValueNull() {
+    // empty
+  }
+}
